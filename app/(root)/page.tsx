ceeb1c9b9e2fd59f5152fb5c69497181487a94bc
mahirgautam <165561059+mahirgautam@users.noreply.github.com>
2025-06-05 00:00:56 +0530

    home-page started

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
+import InterviewCard from '@/components/InterviewCard'
 import { Button } from '@/components/ui/button'
+import { dummyInterviews } from '@/constants'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -24,8 +26,23 @@ const Page = () => {
           <h2>Your Interviews</h2>
 
           <div className='interviews-section'>
-              <p>You haven&apos;t taken any interviews yet</p>
+              {dummyInterviews.map((interview) => (
+  <InterviewCard key={interview.id} {...interview} />
+))}
+
+              {/* <p>You haven&apos;t taken any interview yet</p> */}
           </div>
+      </section>
+
+      <section className='flex flex-col gap-6 mt-8'>
+        <h2>Take an Interview</h2>
+
+        <div className='interview-section'>
+          {dummyInterviews.map((interview, index) => (
+  <InterviewCard key={interview.id} {...interview} />
+))}
+
+        </div>
       </section>
     </>
   )
